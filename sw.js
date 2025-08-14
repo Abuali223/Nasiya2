8366e735e024bd92a356b2fde6e670732a3e2abc
Abuali223 <kamerasony24@gmail.com>
2025-08-14 11:30:23 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,11 @@
-// Simple offline cache for PWA
-const CACHE = 'nasiya-cache-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(
-    keys.filter(k => k!==CACHE).map(k => caches.delete(k))
-  )));
-});
-
-self.addEventListener('fetch', e => {
-  const req = e.request;
+const CACHE='alilazer-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
+self.addEventListener('fetch',e=>{
   e.respondWith(
-    caches.match(req).then(res => res || fetch(req).then(resp => {
-      const copy = resp.clone();
-      if(req.method==='GET' && resp.status===200 && req.url.startsWith(self.location.origin)){
-        caches.open(CACHE).then(c => c.put(req, copy));
-      }
-      return resp;
-    }).catch(()=>caches.match('./index.html')))
+    caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const cp=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return resp;
+    }))
   );
 });
